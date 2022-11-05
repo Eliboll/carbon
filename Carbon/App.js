62f6a06998ee88f10f8859679e7e14b0e98fa556
JoshWard97 <Joshwa97@gmail.com>
2022-11-05 11:24:46 -0500

    Created visual for navbar

--- Carbon/App.js
+++ Carbon/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import NavBar from './pages/NavBar';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Balls!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Balls!!!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <NavBar />
+    </>
   );
 }
 
---
+++ Carbon/pages/NavBar.js
@@ -0,0 +1,53 @@
+
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Entypo, Feather, FontAwesome5} from '@expo/vector-icons'; 
+
+export default function NavBar() {
+    return (
+        <View style={{flexDirection: "row"}}>
+            <View onClick={buttonClicked(1)}>
+                <Entypo name="home" size={52} color="white" style={navStyles.button} />
+            </View>
+
+            <View onClick={buttonClicked(1)}>
+                <Feather name="plus-circle" size={52} color="white" style={navStyles.button}/>
+            </View>
+
+            <View onClick={buttonClicked(1)}>
+                <FontAwesome5 name="history" size={52} color="white" style={navStyles.button}/>
+            </View>
+        </View>
+
+    );
+  }
+
+    function Home()
+    {
+    print("Ello")
+    }
+
+    
+
+    function buttonClicked(num)
+    {
+        if(num == 1)
+        {
+            
+        }
+    }
+
+
+const navStyles = StyleSheet.create({
+    
+    button: {
+        backgroundColor: "black",
+        padding: 10,
+        height: 100,
+        flex: 0,
+        padding:20,
+        width: Dimensions.get('screen').width / 3
+    },
+
+    
+  });
+  
