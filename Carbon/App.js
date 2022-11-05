04b74a5a585c5ca8d3f38a5eed639c8c02d15bb7
Andrew Kim <66102642+ahkim3@users.noreply.github.com>
2022-11-05 13:24:31 -0500

    Start reactive buttons

--- Carbon/App.js
+++ Carbon/App.js
@@ -2,29 +2,47 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import NavBar from './pages/NavBar';
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
     <Text>Balls!!!</Text>
+    <Button
+        title="Go to TestScreen"
+        onPress={() => navigation.navigate('Test')}
+      />
     <StatusBar style="auto" />
   </View>
   );
 }
 
-export default function App() {
+function TestScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+    <Text>Not Balls!!!</Text>
+    <Button
+        title="Go to HomeScreen"
+        onPress={() => navigation.navigate('Home')}
+      />
+    <StatusBar style="auto" />
+  </View>
+  );
+}
+
+export default function App({navigation}) {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
       </Stack.Navigator>
 
-      <NavBar />
+      <NavBar navigationVar={navigation} />
     </NavigationContainer>
   );
 }
--- Carbon/pages/NavBar.js
+++ Carbon/pages/NavBar.js
@@ -1,11 +1,30 @@
-
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Dimensions, StyleSheet, Text, View, Button } from 'react-native';
 import { Entypo, Feather, FontAwesome5} from '@expo/vector-icons'; 
 
-export default function NavBar() {
+var navigation;
+
+export default function NavBar({ navigation }) {
+    // this.navigation = navigation;
+
+    function buttonClicked({navigation}, num)
+    {
+        switch(num) {
+            case 0:
+                navigation.navigate('Home');
+            case 1:
+                navigation.navigate('Test');
+            case 2:
+                navigation.navigate('Test');
+            default:
+
+        }
+    }
+
     return (
         <View style={{flexDirection: "row"}}>
-            <View onClick={buttonClicked(1)}>
+            <View onClick={buttonClicked(0)}>
                 <Entypo name="home" size={52} color="white" style={navStyles.button} />
             </View>
 
@@ -13,29 +32,13 @@ export default function NavBar() {
                 <Feather name="plus-circle" size={52} color="white" style={navStyles.button}/>
             </View>
 
-            <View onClick={buttonClicked(1)}>
+            <View onClick={buttonClicked(2)}>
                 <FontAwesome5 name="history" size={52} color="white" style={navStyles.button}/>
             </View>
         </View>
 
     );
-  }
-
-    function Home()
-    {
-    print("Ello")
-    }
-
-    
-
-    function buttonClicked(num)
-    {
-        if(num == 1)
-        {
-
-        }
-    }
-
+}
 
 const navStyles = StyleSheet.create({
     
@@ -50,5 +53,4 @@ const navStyles = StyleSheet.create({
     },
 
     
-  });
-  
+});
